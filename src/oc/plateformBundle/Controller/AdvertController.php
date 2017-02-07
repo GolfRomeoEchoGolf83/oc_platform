b99bfdc60cee93b92bdb66363e930c6ddf5c07ae
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-02-07 14:31:57 +0100

    setting controller with actions set to routes

--- src/oc/plateformBundle/Controller/AdvertController.php
+++ src/oc/plateformBundle/Controller/AdvertController.php
@@ -8,24 +8,65 @@
 
 namespace oc\plateformBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
 	public function indexAction()
 	{
-		$content = $this
-			->get('templating')
-			->render('ocplateformBundle:Advert:index.html.twig');
+		// if page < 1 shoot an error 404 page
+		if($page < 1) {
+			throw new NotFoundHttpException('Page "' .$page. ' "inexistante.');
+		}
 
-		return new Response($content);
+		// call index template
+		return $this->render('plateformbundle:Advert:index.html.twig');
 	}
 
 	// id parameter set to fit with route
 	public function viewAction($id)
 	{
 		// get id from database then return it to view
-		return new Response("Affiche l'annonce d'id ".$id);
+		return $this->render('plateformbundle:Advert:view.html.twig', array(
+			'id' => $id
+		));
+	}
+
+	public function addAction(Request $request)
+	{
+		// if request is POST it's because user has sent form
+		if ($request->isMethod('POST')) {
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce enregistrée');
+			// redirection to page related to page related to advert
+			return $this->redirectToRoute('oc_plateform_view', array(
+				'id' => 5
+			));
+		}
+		// if request isn't in POST, show form
+		return $this->render('plateformbundle:Advert:add.html.twig');
+	}
+
+	public function editAction($id, Request $request)
+	{
+		// get advert related to its id
+		if($request->isMethod('POST')) {
+			$request->getSession()->getFlashBag()->add('notice', 'Annonce modifiée');
+			return $this->redirectToRoute('oc_platform_view', array(
+				'id' => 5
+			));
+		}
+		return $this->render('platformbundle:Advert:edit.html.twig');
+	}
+
+	public function deleteAction($id)
+	{
+		// get advert related to its id
+		// delete said advert
+
+		return $this->render('platformbundle:Advert:delete.html.twig');
+
 	}
 }
